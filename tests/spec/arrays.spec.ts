b31fb215a4f403482df1697ce91d090e5908699c
Jose Miralles <magicomiralles@gmail.com>
2021-06-21 01:04:14 -0400

    Added maxProfit test and method. Made twoSum test more descriptive.

--- tests/spec/arrays.spec.ts
+++ tests/spec/arrays.spec.ts
@@ -1,5 +1,6 @@
-import * as problems from "../src/arrays";
+import * as arrays from "../src/arrays";
 
+//---START---twoSum
 describe("twoSum", () => {
 
     interface IScenario {
@@ -14,11 +15,38 @@ describe("twoSum", () => {
         {nums: [3,3], target: 6, acceptedOutputs: [ [0,1], [1,0] ]}
     ];
 
-    it("should return correct output.", () => {
-        scenarios.forEach(s => {
-            expect(s.acceptedOutputs).toContain(
-                problems.twoSum(s.nums, s.target)
+    
+    scenarios.forEach(scenario => {
+        it(`Should return ${scenario.acceptedOutputs[0]} or ${scenario.acceptedOutputs[1]} when nums: ${scenario.nums} and target: ${scenario.target}.`, () => {
+            expect(scenario.acceptedOutputs).toContain(
+                arrays.twoSum(scenario.nums, scenario.target)
             );
         })
     });
 });
+//---END---twoSum
+
+//---START---maxProfit
+describe("maxProfit", () => {
+
+    interface IScenario {
+        prices: number[];
+        expectedOutput: number;
+    }
+
+    const scenarios: IScenario[] = [
+        {prices: [7,1,5,3,6,4], expectedOutput: 5}
+    ];
+
+    it("Should return 0 if profit cannot be acheived.", () => {
+        expect(arrays.maxProfit([7,6,4,3,1])).toEqual(0);
+    });
+
+    scenarios.forEach(scenario => {
+        it(`Should return ${scenario.expectedOutput} when 'prices' = [${scenario.prices}]`, () => {
+            expect(arrays.maxProfit(scenario.prices)).toEqual(scenario.expectedOutput);
+        });
+    });
+
+});
+//---END---maxProfit
--- tests/src/arrays.ts
+++ tests/src/arrays.ts
@@ -1,3 +1,4 @@
+//---START---twoSum
 /**
  * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -32,3 +33,23 @@ export function twoSum(nums: number[], target: number): number[] {
 
     return [];
 };
+//---END---twoSum
+
+//---START---maxProfit
+/**
+ * You are given an array prices where prices[i] is the price of a given stock
+ * on the i'th day.
+ * 
+ * You want to maximize your profit by choosing a single day to buy one stock and
+ * choosing a different day in the future to sell that stock.
+ * 
+ * Return the maximum profit you can achieve from this transaction.
+ * If you cannot achieve any profit, return 0.
+ * 
+ * @param prices An array of prices.
+ * @returns The maximum profit acheivablem or 0 if profits are possible.
+ */
+export function maxProfit (prices: number[]): number {
+    return 0;
+}
+//---END---maxProfit
