60a2bf6675e7cb7035ae9447917a36379c466282
Jose Miralles <josemiralles1991@gmail.com>
2021-10-17 13:55:47 -0400

    Added comments to getSum

--- data/src/binary.ts
+++ data/src/binary.ts
@@ -4,9 +4,12 @@
  * two integers without using the operators + and -.
  * 
  * https://leetcode.com/problems/sum-of-two-integers/
+ * Explanation: https://www.youtube.com/watch?v=qq64FrA2UXQ
  * 
  * Input: a = 1, b = 2
  * Output: 3
+ * 
+ * Hint: You need to know bitwise operations to complete this problem (AND, XOR, shift).
  * 
  * @param a First addend
  * @param b Second addend
@@ -14,12 +17,12 @@
  */
 export function getSum(a: number, b: number): number {
     
-    let diff: number, carry: number;
+    let sum: number, carry: number;
 
     while( b ){
-        diff = a ^ b;
-        carry = (a&b) << 1;
-        a = diff;
+        sum = a ^ b; // sum = a XOR b
+        carry = (a & b) << 1; // carry = (a AND B) shifted once to the left.
+        a = sum;
         b = carry;
     }
 
