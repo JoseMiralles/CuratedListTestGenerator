753353a9f7940cfc7eada351382c6017a5bc569e
Jose Miralles <josemiralles1991@gmail.com>
2021-10-22 15:03:29 -0400

    Added LengthOfLIS solution and test. Also added dynamic problems to the generator.json file

--- data/src/dynamic.ts
+++ data/src/dynamic.ts
@@ -85,6 +85,36 @@ export function coinChange(coins: number[], amount: number): number {
 }
 
 //---START---lengthOfLIS
-function lengthOfLIS(nums: number[]): number {
+/**
+ * Given an integer array nums, return the length of the longest strictly increasing subsequence.
+ * 
+ * A subsequence is a sequence that can be derived from an array by deleting some or
+ * no elements without changing the order of the remaining elements.
+ * For example, [3,6,2,7] is a subsequence of the array [0,3,1,6,2,2,7].
+ * 
+ * Expected TC: O(n log(n))
+ * 
+ * https://leetcode.com/problems/longest-increasing-subsequence/
+ * 
+ * Input: nums = [10,9,2,5,3,7,101,18]
+ * Output: 4
+ * Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+ * 
+ * Input: nums = [7,7,7,7,7,7,7]
+ * Output: 1
+ */
+export function lengthOfLIS(nums: number[]): number {
+    
+    let sequence: number[] = [nums[0]];
 
+    nums.forEach((num) => {
+        
+        if (num > sequence[sequence.length - 1]) sequence.push(num);
+        else {
+            const nextBiggerNumIndex = sequence.findIndex(val => val >= num);
+            sequence[nextBiggerNumIndex] = num;
+        }
+    });
+
+    return sequence.length;
 };
