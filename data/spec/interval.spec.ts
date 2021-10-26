1ffc148ae3e72efdee94c14f9d52b18474f95e19
Jose Miralles <josemiralles1991@gmail.com>
2021-10-26 19:23:45 -0400

    Created interval tests and solution

--- data/spec/interval.spec.ts
+++ data/spec/interval.spec.ts
@@ -27,3 +27,23 @@ describe("insert", () => {
         });
     });
 });
+
+//---START---merge
+describe("merge", () => {
+
+    interface IScenario {
+        intervals: number[][];
+        output: number[][];
+    }
+
+    const scenarios: IScenario[] = [
+        { intervals: [[1,3],[2,6],[8,10],[15,18]], output: [[1,6],[8,10],[15,18]] },
+        { intervals: [[1,4],[4,5]], output: [[1,5]] }
+    ];
+
+    scenarios.forEach(s => {
+        it("Should return an array of the non-overlapping intervals that cover all the intervals in the input.", () => {
+            expect(interval.merge(s.intervals)).toEqual(s.output);
+        });
+    });
+});
