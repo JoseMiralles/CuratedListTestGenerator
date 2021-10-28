819bf37882ce78d0b85d43d3c82a49506056eddc
Jose Miralles <josemiralles1991@gmail.com>
2021-10-28 14:28:03 -0400

    Added tests and solution for mergeTwoLists

--- data/spec/linkedList.spec.ts
+++ data/spec/linkedList.spec.ts
@@ -46,3 +46,43 @@ describe("reverseList", () => {
         }
     });
 });
+
+//---START---mergeTwoLists
+describe("mergeTwoLists", () => {
+
+    interface IScenario {
+        l1: number[];
+        l2: number[];
+        output: number[];
+    };
+
+    const scenarios: IScenario[] = [
+        { l1: [1,2,4], l2: [1,3,4], output: [1,1,2,3,4,4] },
+        { l1: [], l2: [], output: [] },
+        { l1: [], l2: [0], output: [0] },
+    ];
+
+    scenarios.forEach(s => {
+
+        if (s.l1.length + s.l2.length < 1) {
+            
+            it ("Should return null when both lists are empty.", () => {
+                expect(linkedList.mergeTwoLists(null, null)).toBeNull();
+            });
+        } else {
+            
+            const list1 = linkedList.arrayToList(s.l1);
+            const list2 = linkedList.arrayToList(s.l2);
+            let temp: linkedList.ListNode | null = linkedList.mergeTwoLists(list1, list2);
+    
+            it ("Should return a merged list.", () => {
+                
+                s.output.forEach(n => {
+                    
+                    expect(temp?.val).toBe(n);
+                    if (temp) temp = temp.next;
+                });
+            });
+        }
+    });
+});
--- data/src/linkedList.ts
+++ data/src/linkedList.ts
@@ -18,7 +18,18 @@ export function reverseList(head: ListNode | null): ListNode | null {
     return left;
 };
 
-//---INCLUDE
+//---NEEDS [ListNode] NEEDS---//
+
+//---START---mergeTwoLists
+export function mergeTwoLists(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    return null;
+};
+
+//---NEEDS [ListNode,arrayToList] NEEDS---//
+
+/** SHARED ITEMS **/
+
+//---START---ListNode
 export class ListNode {
     val: number
     next: ListNode | null
@@ -27,3 +38,17 @@ export class ListNode {
         this.next = (next === undefined ? null : next)
     }
 }
+
+//---START---arrayToList
+export const arrayToList = (arr: number[]): ListNode => {
+
+    let current = new ListNode(arr[0]);
+
+    for (let i = 1; i < arr.length; i++) {
+
+        current.next = new ListNode(arr[i]);
+        current = current.next;
+    }
+
+    return current;
+};
