f321e95d27c5c3121791b3aeba971c9a7cb0bef6
Jose Miralles <josemiralles1991@gmail.com>
2021-10-31 03:43:41 -0400

    Added rotate

--- data/spec/matrix.spec.ts
+++ data/spec/matrix.spec.ts
@@ -44,3 +44,28 @@ describe("spiralOrder", () => {
         });
     });
 });
+
+//---START---rotate
+describe("rotate", () => {
+
+    interface IScenario {
+        input: number[][];
+        output: number[][];
+    }
+
+    const scenarios: IScenario[] = [
+        { input: [[1,2,3],[4,5,6],[7,8,9]], output: [[7,4,1],[8,5,2],[9,6,3]] },
+        { input: [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]], output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]] },
+        { input: [[1]], output: [[1]] },
+        { input: [[1,2],[3,4]], output: [[3,1],[4,2]] }
+    ];
+
+    scenarios.forEach(s => {
+
+        it ("should return the elements in spiral order as an array.", () => {
+            
+            matrix.rotate(s.input); // This mutates the matrix.
+            expect(s.input).toEqual(s.output);
+        });
+    });
+});
--- data/src/matrix.ts
+++ data/src/matrix.ts
@@ -114,3 +114,58 @@ export function spiralOrder(matrix: number[][]): number[] {
     return flattenedOrder;return [];return [];
 };
 //---END---
+
+//---START---rotate
+/**
+ * You are given an n x n 2D matrix representing an image,
+ * rotate the image by 90 degrees (clockwise).
+ * 
+ * You have to rotate the image in-place, which means you have to modify
+ * the input 2D matrix directly. DO NOT allocate another 2D matrix
+ * and do the rotation.
+ * 
+ * https://leetcode.com/problems/rotate-image/
+ * 
+ * Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * Output: [[7,4,1],[8,5,2],[9,6,3]]
+ * 
+ * Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+ * Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+ * 
+ * Input: matrix = [[1]]
+ * Output: [[1]]
+ * 
+ * Input: matrix = [[1,2],[3,4]]
+ * Output: [[3,1],[4,2]]
+ */
+export function rotate(matrix: number[][]): void {
+
+    for(let i = 0; i < matrix.length; i ++) {
+        for(let j = i + 1; j < matrix.length; j ++) {
+           if(i === j) {
+               continue; // It is placed in the right place.
+           }
+           swapValues(matrix, [i, j], [j, i]);
+       }
+       reverse(matrix[i]);
+   }
+};
+
+function swapValues(matrix: number[][], p1: number[], p2: number[]): void {
+    const aux = matrix[p1[0]][p1[1]];
+    matrix[p1[0]][p1[1]] = matrix[p2[0]][p2[1]];
+    matrix[p2[0]][p2[1]] = aux;
+}
+
+function reverse(array: number[]): void {
+    let i = 0;
+    let j = array.length - 1;
+    while(i < j) {
+        const aux = array[i];
+        array[i] = array[j];
+        array[j] = aux;
+        i++;
+        j--;
+    }
+}
+//---END---
