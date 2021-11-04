d48dcab9f5e2514430af8d50b55a1284438a170b
Jose Miralles <josemiralles1991@gmail.com>
2021-11-03 21:26:16 -0400

    Set default settings

--- settings.ts
+++ settings.ts
@@ -1,17 +1,17 @@
 // Types of problems to include in the test.
 // Default: [ "arrays", "binary", "dynamic", "graph", "linkedList", "heap", "interval", "matrix", "strings", "trees" ]
 export const problemTypes = [
-    // "arrays",
-    // "binary",
-    // "dynamic",
-    // "graph",
-    // "linkedList",
-    // "heap",
-    // "interval",
-    // "matrix",
-    // "strings",
+    "arrays",
+    "binary",
+    "dynamic",
+    "graph",
+    "linkedList",
+    "heap",
+    "interval",
+    "matrix",
+    "strings",
     "trees"
 ];
 
 // Total number of problems to be generated.
-export const totalProblems = 100;
+export const totalProblems = 4;
